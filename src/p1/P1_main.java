39114daec57d1f2af7f58ff8ca973a757db8f6d5
fzapp <fzapp@smail.uni-loeln.de>
2025-12-10 12:28:46 +0100

    aenderung_Einfuehrung_Aufgabenblatt8_Aufg1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class P1_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int getNumber(int a) {
+		return a>42 ?  42 :  0;
+	}
+
+}
